15a291745b493190ef32344011df7ab14e4f81ce
= <=>
2020-10-21 15:14:19 +0700

    add TestAnimated.js

--- src/animation/TestAnimated.js
+++ src/animation/TestAnimated.js
@@ -20,20 +20,22 @@ export default class TestAnimated extends Component {
 	componentDidMount() {
 	}
 
-	spring = () => { 
+	spring(w1, w2){ 
 		Animated.sequence([
 			Animated.spring(
 				this.springValue,
 				{
-				  toValue: width,
-				  friction: 10000000,
+				  toValue: w1,
+				  friction: 8,
+				  useNativeDriver: false,
 				}
 			),
 			Animated.spring(
 				this.springValue,
 				{
-				  toValue: 0,
-				  friction: 10000000,
+				  toValue: w2,
+				  friction: (w2 == 0) ? 100 : 4,
+				  useNativeDriver: false,
 				}
 			),
 		]).start();
@@ -43,9 +45,14 @@ export default class TestAnimated extends Component {
 		console.log(this.springValue)
 		return(
 			<View style={{flex: 1, alignItems: 'flex-start', justifyContent: 'center', backgroundColor: '#2f2f2f' }}>
-			  <Text
-			    style={{marginLeft: 10, color: '#f7ac3b', fontSize: 25, fontWeight: '500'}}
-			    onPress={this.spring}>Sliding</Text>
+			  <TouchableOpacity onPress={()=>{
+			  	this.spring(width, width/2);
+			  }}>
+				  <Text
+				    style={{marginLeft: 10, color: '#f7ac3b', fontSize: 40, fontWeight: '500'}}
+				    >Sliding
+				  </Text>
+			  </TouchableOpacity>
 			    <Animated.View
 			    	style={{
 			    		backgroundColor: '#f7ac3b', 
@@ -57,9 +64,14 @@ export default class TestAnimated extends Component {
 					    alignItems: 'flex-end'
 					}}
 			    >
-			    	<Text style={{marginLeft: 10, color: '#2f2f2f', fontSize: 25, fontWeight: '500'}}>
-			    	Sliding
-			    	</Text>
+			    	<TouchableOpacity onPress={()=>{
+			    		this.spring(width, 0);
+			    	}}>
+				    	<Text style={{marginLeft: 10, color: '#2f2f2f', fontSize: 40, fontWeight: '500'}}
+				    	>
+				    	Sliding
+				    	</Text>
+			    	</TouchableOpacity>
 			    </Animated.View>
 			    <Animated.Image
 			      source={{uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/alexanderfarennikov/images/1198519/reactjs.png'}}/>
